9b26a995d9a1dd0bf4669a01479642e8923d525f
SevenPain <nguyenhaanhkiem@gmail.com>
2022-01-09 22:31:09 +0700

    Insert Full Collection

---
+++ src/api/mode-of-payment/controllers/mode-of-payment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  mode-of-payment controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mode-of-payment.mode-of-payment');
---
+++ src/api/product-catalog/routes/product-catalog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-catalog router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-catalog.product-catalog');
